0d56f615cf0857cb0482f5fd1bfe7ae2ccfb6e2d
Song <sssooonnnggg111@gmail.com>
2026-04-28 18:15:47 +0800

    fix: fix pause not breaking after attach without prior breakpoint  DoAction routes pause through hitDebugger, which stays null until a breakpoint actually fires.

--- emmy_debugger/src/emmy_facade.cpp
+++ emmy_debugger/src/emmy_facade.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
 * Copyright (c) 2019. tangzx(love.tangzx@qq.com)
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
@@ -334,6 +334,10 @@ void EmmyFacade::Hook(lua_State *L, lua_Debug *ar) {
 			return;
 		}
 
+		// 没人断着时，把当前活跃 vm 记下来，使未断点过的 pause 能定位到目标
+		if (!_emmyDebuggerManager.GetHitBreakpoint()) {
+			_emmyDebuggerManager.SetHitDebugger(debugger);
+		}
 		debugger->Hook(ar, L);
 	} else {
 		if (workMode == WorkMode::Attach) {
@@ -349,6 +353,9 @@ void EmmyFacade::Hook(lua_State *L, lua_Debug *ar) {
 
 			this->transporter->Send(int(MessageCMD::AttachedNotify), obj);
 
+			if (!_emmyDebuggerManager.GetHitBreakpoint()) {
+				_emmyDebuggerManager.SetHitDebugger(debugger);
+			}
 			debugger->Hook(ar, L);
 		}
 	}
